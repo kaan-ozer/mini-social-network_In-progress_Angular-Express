f372748c0c66cf032a1b934e047dd5ad3dc016a6
kaan-ozer <kaanozer35@gmail.com>
2024-01-30 03:28:01 +0300

    updates for postService

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css',
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
   //   posts = [
   //     { title: 'First Post', content: "This is the first post's content" },
   //     { title: 'Second Post', content: "This is the second post's content" },
   //     { title: 'Third Post', content: "This is the third post's content" },
   //   ];
 
-  @Input() posts: Post[] = [];
+  // @Input() posts: Post[] = [];
+  posts: Post[] = [];
+
+  constructor(public postService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postService.getPosts();
+  }
 }
---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Post } from './post.model';
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+  private posts: Post[] = [];
+
+  private postsUpdated = new Subject<Post[]>();
+
+  getPosts() {
+    return [...this.posts];
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { title: title, content: content };
+    this.posts.push(post);
+  }
+}
