7f5e46485e30e8584d6aefbf348767fe5afd6150
kaan-ozer <kaanozer35@gmail.com>
2024-01-31 03:54:56 +0300

    express app is wired up with server.js

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log("First middleware");
   next();
 });
 app.use((req, res, next) => {
-  res.send("Hi from express!");
+  console.log("Second middleware");
+  res.send("Hello from express");
 });
-
-app.listen(4040);
-
 
 module.exports = app;
