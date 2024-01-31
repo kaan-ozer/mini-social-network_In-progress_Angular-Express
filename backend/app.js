ebdbf41a8ccba55708bf4e9ecd68be9465e9b59f
kaan-ozer <kaanozer35@gmail.com>
2024-01-31 04:52:39 +0300

    server.js updated and first restfulApi service end point posts created

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("Second middleware");
-  res.send("Hello from express");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "dadfadsf",
+      title: "First server-side post",
+      content: "This is coming from the server",
+    },
+    {
+      id: "ksajdfklsaj",
+      title: "Second server-side post",
+      content: "This is coming from the server!",
+    },
+  ];
+
+  //there is no need for return because it is the last statement in this use function so it will return automatically
+  res
+    .status(200)
+    .json({ message: "Posts fetched successfully!", posts: posts });
 });
 
 module.exports = app;
